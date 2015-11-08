947f55bc42086c15e7560c4d2b2fa299d9a028fc
yuklviv <yuk.lviv@gmail.com>
2015-11-09 01:48:05 +0200

    Task 3 06 modified

--- Task 3_06.py
+++ Task 3_06.py
@@ -1,17 +1,20 @@
-print('Type n,m > 5')
+print('Type the array size (N x M) where n,m > 5')
 n = int(input('n='))
 m = int(input('m='))
 if n and m > 5:
     Amt = [ ]
     for i in range(n):
-        Amt.append([int(input('Type element=')) for j in range (m)])
-    
+        row = raw_input('Type the ' + str(m) + ' elements row ' + str(i+1) + ': ').split()
+        for i in range(len(row)):
+            row[i] = int(row[i])
+        Amt.append(row)
+        
     for i in range (n):
         Amt[i].sort()
    
     Amt.sort(reverse=True) 
-    for q in range (len(Amt)):
-        print(Amt[q])    
+    for i in range (len(Amt)):
+        print(Amt[i])    
 else:
     print('Error! n or m <= 5')
     
