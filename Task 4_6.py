e190dd88f1edd94ff3ea36a0daac4f232e59cd16
yuklviv <yuk.lviv@gmail.com>
2015-11-12 21:41:15 +0200

    Task 04//06

---
+++ Task 4_6.py
@@ -0,0 +1,18 @@
+a = (raw_input('List: '))
+c = []
+a = a.replace(' ','')
+
+if len(a)<253:
+    for i in a:
+        c.append(i)
+    c.sort()
+    
+    for i in range(len(c)):
+        if c[i-1] != c[i]:
+            d = c.count(c[i])
+            print c[i],':',d
+        
+else:
+    print ('Error. Lenth is bigger than possible')
+
+
