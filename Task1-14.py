675a638cc30c7ab184127c7b57b6cf58e2f9bfa7
vusatchy <vusatch.y@gmail.com>
2016-02-11 22:19:33 +0200

    Delete

--- Task1-14.py
+++
@@ -1,8 +0,0 @@
-#формула - ((-1)**n)*n**2
-n=int(input('введіть перше число '))
-lastnumber=int(input('введіть останнє число '))
-s=0
-while n<=lastnumber:
-    s=s+((-1)**n)*n**2
-    n=n+1
-print('сума дорівнює', s)
--- Task6-14.py
+++
@@ -1,9 +0,0 @@
-a=int(input('write a numder  '))
-def ln(z,n=1):
-    x=(z-1)/(z+1)
-    if n>1000:
-        return 2*((x**n)/n)
-    else:
-        s=2*((x**n)/n)+ln(z,n=n+2)
-        return s
-print(round(ln(a),3))
