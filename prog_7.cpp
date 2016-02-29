2e10c39a200a4959021a6bc3d7abcfb6ca5f41e2
irynaskyba <irynaskyba97@gmail.com>
2016-02-29 21:52:54 +0200

    Create prog_7.cpp

---
+++ prog_7.cpp
@@ -0,0 +1,21 @@
+// All common multiples of two numbers (m,n)
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main()
+{
+	int n, m, k; // k - common multiple
+	cout << "m = ";
+	cin >> m;
+	cout << "n = ";
+	cin >> n;
+	for (k = 1; k < m*n; k++)
+	{
+		if (m % k == 0 && n % k == 0)
+		{
+			cout << k << endl;
+		}
+	}
+	_getch();
+}
